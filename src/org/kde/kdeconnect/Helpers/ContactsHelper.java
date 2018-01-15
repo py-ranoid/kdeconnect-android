6da5104a1f85930b799dbabb3c75db604056d25a
Simon Redman <simon@ergotech.com>
2018-01-15 00:16:39 -0700

    Add method to ContactsHelper for extracting information from the contacts database given raw contact ids

--- src/org/kde/kdeconnect/Helpers/ContactsHelper.java
+++ src/org/kde/kdeconnect/Helpers/ContactsHelper.java
@@ -39,6 +39,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ContactsHelper {
 
@@ -163,6 +164,134 @@ public class ContactsHelper {
                 toReturn.add(contactID);
             } while (contactsCursor.moveToNext());
             try { contactsCursor.close(); } catch (Exception e) {}
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * Return a mapping of raw contact IDs to a map of the requested data from the Contacts database
+     *
+     * If for some reason there is no row associated with the raw contact ID in the database,
+     * there will not be a corresponding field in the returned map
+     *
+     * @param context android.content.Context running the request
+     * @param IDs collection of raw contact IDs to look up
+     * @param contactsProjection List of column names to extract, defined in ContactsContract.Contacts
+     * @return mapping of raw contact IDs to desired values, which are a mapping of column names to the data contained there
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static Map<Long, Map<String, Object>> getColumnsFromContactsForRawContactIDs(Context context, Set<Long> IDs, String[] contactsProjection)
+    {
+        HashMap<Long, Map<String, Object>> toReturn = new HashMap<>();
+
+        // Define the columns we want to read from the RawContacts database
+        final String[] rawContactsProjection = new String[] {
+                ContactsContract.RawContacts._ID,
+                ContactsContract.RawContacts.CONTACT_ID
+        };
+
+        Uri rawContactsUri = ContactsContract.RawContacts.CONTENT_URI;
+        Uri contactsUri = ContactsContract.Contacts.CONTENT_URI;
+
+        Cursor rawContactsCursor = context.getContentResolver().query(
+                rawContactsUri,
+                rawContactsProjection,
+                null,
+                null,
+                null);
+
+        if (rawContactsCursor != null && rawContactsCursor.moveToFirst())
+        {
+            do {
+                Long rawContactID;
+                Long contactID;
+
+                int rawContactIDIndex = rawContactsCursor.getColumnIndex(ContactsContract.RawContacts._ID);
+                if (rawContactIDIndex != -1) {
+                    rawContactID = rawContactsCursor.getLong(rawContactIDIndex);
+                } else {
+                    // This raw contact didn't have an ID? Something is very wrong.
+                    // TODO: Investigate why this would happen
+                    Log.e("ContactsHelper", "Got a raw contact which does not have an _ID");
+                    continue;
+                }
+
+                // Filter only for the rawContactIDs we were asked to look up
+                if (! IDs.contains(rawContactID))
+                {
+                    // This should be achievable (and faster) by providing a selection
+                    // and selectionArgs when fetching rawContactsCursor, but I can't
+                    // figure that out
+                    continue;
+                }
+
+                int contactIDIndex = rawContactsCursor.getColumnIndex(ContactsContract.RawContacts.CONTACT_ID);
+                if (contactIDIndex != -1) {
+                    contactID = rawContactsCursor.getLong(contactIDIndex);
+                } else {
+                    // Something went wrong with this contact
+                    // TODO: Investigate why this would happen
+                    Log.e("ContactsHelper", "Got a raw contact which does not have a CONTACT_ID");
+                    continue;
+                }
+
+                // Filter on only the contact we are interested in
+                final String contactsSelection = ContactsContract.Contacts._ID + " == ? ";
+                final String[] contactsArgs = new String[] {contactID.toString()};
+
+                Cursor contactsCursor = context.getContentResolver().query(
+                        contactsUri,
+                        contactsProjection,
+                        contactsSelection,
+                        contactsArgs, null
+                );
+
+                Map<String, Object> requestedData = new HashMap<>();
+
+                if (contactsCursor != null && contactsCursor.moveToFirst())
+                {
+                    // For each column, collect the data from that column
+                    for (String column : contactsProjection)
+                    {
+                        int index = contactsCursor.getColumnIndex(column);
+                        // Since we might be getting various kinds of data, Object is the best we can do
+                        Object data;
+                        int type;
+                        if (index == -1) {
+                            // This raw contact didn't have an ID? Something is very wrong.
+                            // TODO: Investigate why this would happen
+                            Log.e("ContactsHelper", "Got a raw contact which does not have an _ID");
+                            continue;
+                        }
+
+                        type = contactsCursor.getType(index);
+                        switch(type) {
+                            case Cursor.FIELD_TYPE_INTEGER:
+                                data = contactsCursor.getInt(index);
+                                break;
+                            case Cursor.FIELD_TYPE_FLOAT:
+                                data = contactsCursor.getFloat(index);
+                                break;
+                            case Cursor.FIELD_TYPE_STRING:
+                                data = contactsCursor.getString(index);
+                                break;
+                            case Cursor.FIELD_TYPE_BLOB:
+                                data = contactsCursor.getBlob(index);
+                                break;
+                            default:
+                                Log.e("ContactsHelper", "Got an undefined type of column " + column);
+                                continue;
+                        }
+
+                        requestedData.put(column, data);
+                    }
+                }
+                contactsCursor.close();
+
+                toReturn.put(rawContactID, requestedData);
+            } while (rawContactsCursor.moveToNext());
+            rawContactsCursor.close();
         }
 
         return toReturn;
